cd75178f5453c153ebc6a37962dc82803be1830a
carlosACE98 <158324649+carlosACE98@users.noreply.github.com>
2024-10-28 20:37:20 -0600

    Add files via upload

---
+++ Peer-graded Assisgnment.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# # Peer-graded Assignment
+
+# ## Introduction
+# In this peer graded assignemtn I will apply all my learning in this course, in order to succed in this submition.
+
+# **Objectives**
+# 
+# - Create the two types of lists.
+# - Create tables in markdown.
+# - Upload your job into a gitHub repo.
+
+# ## Data Science languages:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Scala
+# 5. Rubi
+# 6. Java
+
+# ## Data Science libraries:
+# 1. Pandas
+# 2. NumPy
+# 3. Matplotlib
+# 4. Seaborn
+# 5. Scikit-learn
+# 6. Keras
+# 7. Tensorflow
+# 8. PyTorch
+
+# ## Data Science tools table.
+# | Data Science Tools |
+# | ----------------- |
+# | <center>MySQL</center> |
+# | <center>Cassandra</center> |
+# | <center>MongoDB</center> |
+# | <center>Airflow</center> |
+# | <center>Kubeflow</center> |
+# | <center>Kafka</center> |
+# | <center>PixieDust</center> |
+# | <center>Kibana</center> |
+# | <center>Seldon</center> |
+# | <center>GitHub</center> |
+# | <center>GitLab</center> |
+
+# ## There are some arithmetic examples we can do with Jupyter notebooks.
+
+# In[2]:
+
+
+# This is a multiplication
+6 * 20
+
+
+# In[3]:
+
+
+# This is an addition 
+407 + 98
+
+
+# ### Lets convert minutes to hours. One hour has 60 minutes, so lets try to convert 300 minutes to hours
+
+# In[4]:
+
+
+300 / 60
+
+
+# ## Author
+# ***Carlos Alberto Calderón Espinosa***
+
+# Copyright ©2024 IBM Corporation. All rights reserved.
+# 
